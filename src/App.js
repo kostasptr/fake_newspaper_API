e0ed47a0a76f208ac0d987aa4e9dec9dc6291873
kostasptr <kostas-ptr@hotmail.com>
2021-12-06 00:33:36 +0200

    route for github added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="wrapper">
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/fake_newspaper_API/" element={<Home/>} />
           <Route path="*" element={<NotFound/>} />
         </Routes>        
       </div>
